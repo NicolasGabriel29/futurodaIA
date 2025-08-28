e9a109d71dd852bf83481a43b71293a5cff3825c
NicolasGabriel29 <145770976+NicolasGabriel29@users.noreply.github.com>
2025-08-28 11:46:16 -0300

    ouygo

--- script.js
+++ script.js
@@ -3,3 +3,28 @@ const caixaPerguntas = document.querySelector(".caixa-pergunta");
 const caixaAlternativas = document.querySelector(".caixa-alternativa");
 const caixaResultado = document.querySelector(".caixa-resultados");
 const caixaResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado:
+        "Você usa a IA no seu trabalho?"
+        alternativas:["Sim", "Não"],
+    },
+    {
+        enunciado:
+        "Você acha que os avanços da IA afetarão a privacidade e a liberdade individual no futuro?"
+        alternativas: ["Sim","Não"],
+    }
+    { 
+        enunciado:
+        "A IA poderá ser usada para resolver grandes problemas globais, como mudanças climáticas e desilgualdade social?"
+        alternativas: ["Sim","Não"],
+    }
+    {
+        enunciado:
+        "A IA poderá desenvolver consciência ou emoções senelhantes às humanas?"
+        alternativas: ["Sim","Não"],
+    }
+    {enunciado:
+        "A IA impactará o mercado de trabalho nos proximos 10 anos?"
+        alternativas: ["Sim","Não"],}
