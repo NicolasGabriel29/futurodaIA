3895f206528d9aee20fab48a0c5dcee5d439dc1c
NicolasGabriel29 <145770976+NicolasGabriel29@users.noreply.github.com>
2025-08-28 11:03:05 -0300

    i766f

---
+++ script.js
@@ -0,0 +1,5 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-pergunta");
+const caixaAlternativas = document.querySelector(".caixa-alternativa");
+const caixaResultado = document.querySelector(".caixa-resultados");
+const caixaResultado = document.querySelector(".caixa-resultado");
