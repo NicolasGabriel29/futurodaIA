1d4f13d81f61831ec6349a5eec4089930c40533a
NicolasGabriel29 <145770976+NicolasGabriel29@users.noreply.github.com>
2025-09-11 11:23:31 -0300

    i76f7

--- script.js
+++ script.js
@@ -42,10 +42,17 @@ const perguntas = [
     function mostraAlternativas() {
         for (const alternativa of perguntaAtual.alternativas) {
             const botaoAlternativas = document.createElement("button");
-            botaoAlternativas.textContent = alternativa;
+            botaoAlternativas.textContent = alternativa.texto;
+            botaoAlternativas.addEventListener("click"),() => respostaSelecionada(alternativa);
             caixaAlternativas. appendChild(botaoAlternativas);
         }
     }
 
+    function respostaSelecionada(opcaoSelecionada.afirmacoes){
+        const afirmacoes = opcaoSelecionada.afirmacoes;
+        historiaFinal = afirmacoes;
+        atual++;
+        mostraPergunta();
+    }
+
     mostraPergunta();
-]
